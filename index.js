adf3ed724a9891ebcef535e25d0e342cf0d76790
leofree1 <leoinline1@hotmail.com>
2021-04-25 21:45:17 -0300

    Correções front

--- index.js
+++ index.js
@@ -6,55 +6,55 @@ var controleCadastro = {};
 var controleUsuario = "";
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
-app.use(express.static(__dirname +'/public'));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
 app.get("/home", (req, res) => {
-    res.render("../views/home", {usuario: controleUsuario});
+    res.render("../views/home", { usuario: controleUsuario });
 });
 
-app.get("/cadastro", (req,res) => {
-    res.render("../views/cadastro", {usuario: controleUsuario});
+app.get("/cadastro", (req, res) => {
+    res.render("../views/cadastro", { usuario: controleUsuario });
 });
 
-app.get("/deslogar", (req,res) => {
+app.get("/deslogar", (req, res) => {
     controleUsuario = "";
     controleCadastro = {};
-    res.render("../views/home", {usuario: controleUsuario});
+    res.render("../views/home", { usuario: controleUsuario });
 });
 
-app.get("/comentario", (req,res) => {
-    if(!controleCadastro.id) res.render("../views/verificarCadastro", {usuario: controleUsuario});
-    else{
+app.get("/comentario", (req, res) => {
+    if (!controleCadastro.id) res.render("../views/verificarCadastro", { usuario: controleUsuario });
+    else {
 
         let listaComentarios = [];
         let comentarios;
-    
-        function buscaComentarios(){
+
+        function buscaComentarios() {
             return axios.post("http://138.68.7.94:85/busca_comentarios")
         }
-    
+
         comentarios = buscaComentarios();
-    
-        comentarios.then(response =>{
-            listaComentarios = response.data.slice(0,100);
-            res.render("../views/paginaComentarios", {dadosComentarios : listaComentarios, usuario: controleUsuario})
+
+        comentarios.then(response => {
+            listaComentarios = response.data.slice(0, 100);
+            res.render("../views/paginaComentarios", { dadosComentarios: listaComentarios, usuario: controleUsuario })
         })
     }
 });
 
-app.post("/cadastrarUsuario", (req,res) => {
+app.post("/cadastrarUsuario", (req, res) => {
 
     let cadastrarUsuario = {
         email: req.body.email,
-        nome: req.body.nome 
+        nome: req.body.nome
     };
 
     let dadosUsuario;
 
-    function cadastroUsuario(){
+    function cadastroUsuario() {
         return axios.post("http://138.68.7.94:85/cadastro", cadastrarUsuario)
     }
 
@@ -64,39 +64,38 @@ app.post("/cadastrarUsuario", (req,res) => {
         dadosUsuario = response.data;
         controleCadastro = dadosUsuario;
         controleUsuario = controleCadastro.nome.toString();
-        res.render("../views/cadastroSucesso", {cadastro : dadosUsuario});
+        res.render("../views/cadastroSucesso", { cadastro: dadosUsuario });
     }).catch(err => {
-        if(err)
-        {
-            res.render("../views/cadastroErro", {erro : err.response.data, usuario: controleUsuario});
+        if (err) {
+            res.render("../views/cadastroErro", { erro: err.response.data, usuario: controleUsuario });
         }
     })
 
 });
 
-app.post("/cadastrarComentario", (req,res) => {
+app.post("/cadastrarComentario", (req, res) => {
 
     let cadastraComentario = {
         id_usuario: controleCadastro.id,
-        comentario: req.body.comentario 
+        comentario: req.body.comentario
     };
 
     axios.post("http://138.68.7.94:85/add_comentario", cadastraComentario).then(response => {
         let listaComentarios = [];
         let comentarios;
-    
-        function buscaComentarios(){
+
+        function buscaComentarios() {
             return axios.post("http://138.68.7.94:85/busca_comentarios")
         }
-    
+
         comentarios = buscaComentarios();
-    
-        comentarios.then(response =>{
-            listaComentarios = response.data.slice(0,100);
-            res.render("../views/paginaComentarios", {dadosComentarios : listaComentarios, dadosUsuario: controleCadastro, usuario: controleUsuario})
+
+        comentarios.then(response => {
+            listaComentarios = response.data.slice(0, 100);
+            res.render("../views/paginaComentarios", { dadosComentarios: listaComentarios, dadosUsuario: controleCadastro, usuario: controleUsuario })
         })
     }).catch(err => {
-        res.render("../views/comentarioErro", {erro : err.response.data}, {usuario: controleUsuario});
+        res.render("../views/comentarioErro", { erro: err.response.data }, { usuario: controleUsuario });
     })
 
 });
